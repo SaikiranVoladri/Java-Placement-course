c6350e2872728adc4900107a9123606fb65cf2c8
Saikiran voladri <200303124537@paruluniversity.ac.in>
2022-08-30 18:59:26 +0530

    changed code

--- 12(arrays)/binarysearch.java
+++ 12(arrays)/binarysearch.java
@@ -1,6 +1,7 @@
 public class binarysearch {
     public static int binarysearch(int arr[],int key){
-        int start=0;int end=arr.length-1;
+        int start=0;
+        int end=arr.length-1;
         while(start<=end){
             int mid=(start+end)/2;
             if(arr[mid]==key){
