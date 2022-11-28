e3741536fee2be6505f970a24c0e2700ede79d02
Saikiran voladri <200303124537@paruluniversity.ac.in>
2022-11-28 08:51:34 +0530

    issues cleared

--- 22(LinkedListPart1)/mergesortinLL.java
+++ 22(LinkedListPart1)/mergesortinLL.java
@@ -28,7 +28,7 @@ public class mergesortinLL {
     }
     public Node getmid(Node head){
         Node slow=head;
-        Node fast=head.next;;
+        Node fast=head.next;
         while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
