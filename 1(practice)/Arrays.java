d7f1866f3c56ee4ddd691584c21a0aba7ac8f89a
Saikiran voladri <200303124537@paruluniversity.ac.in>
2023-06-10 00:46:14 +0530

    Update Arrays.java

--- 1(practice)/Arrays.java
+++ 1(practice)/Arrays.java
@@ -1,43 +1,43 @@
-import java.util.*;
-public class Arrays {
+// import java.util.*;
+// public class Arrays {
 
-    // public static void change(int arr[], int i, int val){
+//     // public static void change(int arr[], int i, int val){
 
-    //     if(i==arr.length){
-    //         printarr(arr);
-    //         return ;
-    //     }
-    //     arr[i]=val;
-    //     change(arr, i+1, val+1);
-    //     arr[i]= arr[i]*10;
-    // }
+//     //     if(i==arr.length){
+//     //         printarr(arr);
+//     //         return ;
+//     //     }
+//     //     arr[i]=val;
+//     //     change(arr, i+1, val+1);
+//     //     arr[i]= arr[i]*10;
+//     // }
 
-    // public static void printarr(int arr[]){
-    //     for(int i=0;i<arr.length;i++){
-    //         System.out.print(arr[i]+" ");
-    //     }
-    //     System.out.println();
-    // }
+//     // public static void printarr(int arr[]){
+//     //     for(int i=0;i<arr.length;i++){
+//     //         System.out.print(arr[i]+" ");
+//     //     }
+//     //     System.out.println();
+//     // }
    
 
-    public static int rev(int n){
-        int reverse=0;
+//     public static int rev(int n){
+//         int reverse=0;
       
-       while(n>0){
-        int nbr=n%10;
-        reverse=(reverse*10)+nbr;
-        n=n/10; 
-       }
-       return reverse;
-    }
-    public static void main(String[] args) {
+//        while(n>0){
+//         int nbr=n%10;
+//         reverse=(reverse*10)+nbr;
+//         n=n/10; 
+//        }
+//        return reverse;
+//     }
+//     public static void main(String[] args) {
       
-       System.out.println(rev(1234));
+//        System.out.println(rev(1234));
 
 
       
         
        
-    }
+//     }
  
-}
+// }
