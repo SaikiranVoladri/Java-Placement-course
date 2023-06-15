77e440f4c006f82c861287e2c38763b91f2e49be
Saikiran voladri <200303124537@paruluniversity.ac.in>
2023-06-15 12:54:21 +0530

    Update assq1.java

--- 10(functions&methods)/assq1.java
+++ 10(functions&methods)/assq1.java
@@ -1,15 +1,15 @@
-// import java.util.*;
-//  write a program to average of three number
-// public class assq1 {
-//     public static int printAvg(int a,int b,int c){
-//          int avg=(a+b+c)/3;
-//          return avg;
+import java.util.*;
+ //write a program to average of three number
+public class assq1 {
+    public static int printAvg(int a,int b,int c){
+         int avg=(a+b+c)/3;
+         return avg;
         
-//     }
-//     public static void main(String args[])
-//     {
+    }
+    public static void main(String args[])
+    {
     
-//      System.out.println("average ="+ printAvg(3,5,5) );
-//     }
+     System.out.println("average ="+ printAvg(3,5,5) );
+    }
     
-// }
+}
