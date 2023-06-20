03c75af83a95b297ae65c5a28c3cf484e9403402
Saikiran voladri <200303124537@paruluniversity.ac.in>
2023-06-20 21:18:42 +0530

    Update floidstriangle.java

--- 11(advancedpatterns)/floidstriangle.java
+++ 11(advancedpatterns)/floidstriangle.java
@@ -1,7 +1,7 @@
 //public class floidstriangle {
    // public static void floidstriangle(int n){
         //int count=1;
-        for(int i=1;i<=n;i++){
+        //for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(count + " ");
                 count++;
