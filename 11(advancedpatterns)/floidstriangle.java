ac6e521c538af40c7a030d91c0368fb2adefb35e
Saikiran voladri <200303124537@paruluniversity.ac.in>
2023-06-20 21:19:07 +0530

    Update floidstriangle.java

--- 11(advancedpatterns)/floidstriangle.java
+++ 11(advancedpatterns)/floidstriangle.java
@@ -4,7 +4,7 @@
         //for(int i=1;i<=n;i++){
            // for(int j=1;j<=i;j++){
                 //System.out.print(count + " ");
-                count++;
+               // count++;
             }
             System.out.println();
         }
