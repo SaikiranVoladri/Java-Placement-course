4ff2d355ec211e6792ee1f21e9ef440a4d3725cf
Saikiran voladri <saikiranreddy01818@gmail.com>
2023-06-24 18:11:29 +0530

    Update deleteaNode.java

--- 28(Binary Search trees part-1)/deleteaNode.java
+++ 28(Binary Search trees part-1)/deleteaNode.java
@@ -3,7 +3,7 @@
            // int data;
           // Node left;
             // Node right;
-            Node(int data){
+           // Node(int data){
          //       this.data= data;
           //      this.left=null;
                 this.right=null;
